89ffc022b5d116ebbe0bfae24f3a61dd633bac5d
Zhijie Wang <zjwa127@gmail.com>
2022-03-18 23:11:22 +0800

    Update 201_esp32.ino
    remove ppm input.
    ready for calibration

--- 201_esp32.ino
+++ 201_esp32.ino
@@ -11,14 +11,12 @@
 Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
 
 SimpleKalmanFilter simpleKalmanFilter1(2, 2, 0.01);
-SimpleKalmanFilter simpleKalmanFilter2(2, 2, 0.01);
 
 // Serial output refresh time
 const long SERIAL_REFRESH_TIME = 100;
 long refresh_time;
 int16_t adc0, adc1, adc2, adc3;
 float flow_base;
-bool 201_state = 1;
 void setup() {
   Serial.begin(115200);
   ads.begin();
@@ -28,17 +26,12 @@ void loop() {
 
   // read a reference value from A0 and map it from 0 to 100
   adc0 = ads.readADC_SingleEnded(0);
-  adc1 = ads.readADC_SingleEnded(1);
-
   // calculate the estimated value with Kalman Filter
   float flow = simpleKalmanFilter1.updateEstimate(adc0);
-  float ppm = simpleKalmanFilter2.updateEstimate(adc1);
-
-if ((flow - flowbase)/flowbase > 0.25){
-  // turn the 201 off
-}
-if (201_state & ppm < 1){
-  //wait for 3 mins and turn the 201 on
-}
+  Serial.printf("flow v = %.2f v. \r\n", flow);
+  
+  if ((flow - flow_base) / flow_base > 0.25) {
+    // turn the 201 off
+  }
 
 }
