b7c7a219d42699271274fd1eafe53952eac1939c
Zhijie Wang <zjwa127@gmail.com>
2022-05-17 10:37:27 +0800

    Add files via upload

--- 201_esp32.ino
+++ 201_esp32.ino
@@ -1,49 +1,47 @@
 #include <SimpleKalmanFilter.h>
 #include <Wire.h>
 #include "Adafruit_ADS1015.h"
-/*
-  SimpleKalmanFilter(e_mea, e_est, q);
-  e_mea: Measurement Uncertainty
-  e_est: Estimation Uncertainty
-  q: Process Noise
-*/
-// Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
 Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
 int buttonState = 0;
 
+int inputPin = 2;
+int outputPin = 5;
 SimpleKalmanFilter simpleKalmanFilter1(2, 2, 0.01);
 
 // Serial output refresh time
 const long SERIAL_REFRESH_TIME = 100;
 long refresh_time;
-int16_t adc0, adc1, adc2, adc3;
+int16_t adc0, adc1, adc2, adc3, voltage;
 float flow_base;
 void setup() {
   Serial.begin(115200);
   ads.begin();
-  pinMode(5, OUTPUT);
-  pinMode(4, INPUT);
+  ads.setGain(GAIN_ONE);
+  pinMode(outputPin, OUTPUT);
+  pinMode(inputPin, INPUT);
 }
 
 void loop() {
-  buttonState = digitalRead(4);
+  buttonState = digitalRead(inputPin);
   // read a reference value from A0 and map it from 0 to 100
   adc0 = ads.readADC_SingleEnded(0);
-  // calculate the estimated value with Kalman Filter
   float flow = simpleKalmanFilter1.updateEstimate(adc0);
-  Serial.printf("flow v = %.2f v. \r\n", flow);
+  Serial.println(buttonState);
+  Serial.println(adc0);
+  Serial.printf("flow=  '', v = %.2f v. \r\n", flow);
 
   //cases:
-  if ((flow - flow_base) / flow_base > 0.25) {
+  if (flow < 1) {
     // turn the 201 off
-    digitalWrite(5, LOW);
+    digitalWrite(outputPin, LOW);
   }
 
-  if(buttonState == HIGH){
-    Serial.println("Turning ON after 3 minutes.");
-    delay(180000);
-    digitalWrite(5,HIGH);
+  if (buttonState == HIGH) {
+    Serial.println("Turning ON after 5 seconds.");
+    delay(5000);
+    digitalWrite(outputPin, HIGH);
   }
-  
+  delay(500);
 
 }
