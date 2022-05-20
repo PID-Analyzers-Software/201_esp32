424cccbb5862a3bbd1d7e8c739e3541e8d4ccce5
Zhijie Wang <zjwa127@gmail.com>
2022-05-21 01:57:22 +0800

    Update 201_esp32.ino

--- 201_esp32.ino
+++ 201_esp32.ino
@@ -4,16 +4,16 @@
 
 Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
 int buttonState = 0;
-float v_lowLimit = 1070, voltage;
+float v_lowLimit = 1070, voltage0, voltage1;
 int inputPin = 4;
 int outputPin = 5;
 SimpleKalmanFilter simpleKalmanFilter1(2, 2, 0.01);
+//SimpleKalmanFilter simpleKalmanFilter2(2, 2, 0.01);
 
 // Serial output refresh time
 const long SERIAL_REFRESH_TIME = 100;
 long refresh_time;
 int16_t adc0, adc1, adc2, adc3;
-float flow_base;
 void setup() {
   Serial.begin(115200);
   ads.begin();
@@ -28,11 +28,11 @@ void loop() {
   adc0 = ads.readADC_SingleEnded(0);
   adc1 = ads.readADC_SingleEnded(1);
   float voltage = simpleKalmanFilter1.updateEstimate(adc0 * 2);
-
-  Serial.printf("flow=  %.2f mV. adc1 = %.2f mV.\r\n", voltage, adc1);
-  if (adc1 > 100) {
+  float voltage2 = adc1 * 2;
+  Serial.printf("flow=  %.2f mV. adc1 = %.2f mV.\r\n", voltage, voltage2);
+  if (voltage2 > 900) {
     Serial.println("Input High");
-  } else{
+  } else {
     Serial.println("Input Low");
   }
   //cases:
@@ -41,11 +41,9 @@ void loop() {
     digitalWrite(outputPin, LOW);
     Serial.println("Turning OFF");
     delay(200);
-  } 
-
-  if (adc1 > 100) {
-    Serial.println("Turning ON after 3 minutes.");
-    delay(3  * 1000);
+  } else if (voltage2 > 900) {
+    Serial.println("Turning ON after 10 seconds (test).");
+    delay(10 * 1000);
     digitalWrite(outputPin, HIGH);
   }
   delay(50);
