f78ef905ab28b7a2cf4beeb0eae475fc8bd68a03
Zhijie Wang <zjwa127@gmail.com>
2023-01-20 23:04:35 +0800

    Update 201_esp32.ino

--- 201_esp32.ino
+++ 201_esp32.ino
@@ -22,9 +22,9 @@ void setup() {
   ads.setGain(GAIN_ONE);
   pinMode(outputPin, OUTPUT);
   digitalWrite(outputPin, HIGH);
-  Serial.println("5 Minutes delay started");
-  delay(5 * 60 * 1000);
-  Serial.println("5 Minutes delay finished");
+  Serial.println("Start up 3 Minutes delay started");
+  delay(3 * 60 * 1000);
+  Serial.println("Start up 3 Minutes delay finished");
   for (int i = 0; i <= 20; i++) {
     adc0 = ads.readADC_SingleEnded(0);
     voltage0 = simpleKalmanFilter1.updateEstimate(1050);
